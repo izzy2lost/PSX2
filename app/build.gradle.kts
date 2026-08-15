5149abac098f5c5b418cec83ad9d162f117c6d03
izzy2lost <izzynochill@gmail.com>
2026-08-15 15:14:12 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.izzy2lost.psx2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.2.7"
+        versionCode = 30
+        versionName = "1.2.8"
 
         externalNativeBuild {
             cmake {
